b32d50972472faed7770a5695d6b797f1d0fef11
nicolas <nicolas.eymenier@gmail.com>
2021-01-03 22:42:12 -0300

    ok hide non interesting language

--- app/javascript/plugins/init_switch_language.js
+++ app/javascript/plugins/init_switch_language.js
@@ -9,42 +9,51 @@ const initSwitchLanguage = () => {
   const portuguese_flag = document.getElementById('Português');
   const french_flag = document.getElementById('Français');
 
+  var containers = document.querySelectorAll('.container');
+
   if (spanish_flag) {
     spanish_flag.addEventListener('click', (e) => {
       console.log("spanish")
+
+        containers.forEach((container) => {
+        // console.log(container.id);
+        if (!(container.classList.contains("hidden-style"))) {
+            container.classList.add("hidden-style");
+        };
+      });
+
         if (spanish_page.classList.contains("hidden-style")) {
             spanish_page.classList.remove("hidden-style");
-
-
+      };
     });
-  }
+  };
 
   if (english_flag) {
     english_flag.addEventListener('click', (e) => {
-      console.log("spanish")
+      console.log("english")
         if (english_page.classList.contains("hidden-style")) {
             english_page.classList.remove("hidden-style");
+      };
     });
-
-  }
+  };
 
    if (french_flag) {
     french_flag.addEventListener('click', (e) => {
-      console.log("spanish")
+      console.log("french")
         if (french_page.classList.contains("hidden-style")) {
             french_page.classList.remove("hidden-style");
+      };
     });
-
-  }
+  };
 
     if (portuguese_flag) {
     portuguese_flag.addEventListener('click', (e) => {
-      console.log("spanish")
+      console.log("portuguese")
         if (portuguese_page.classList.contains("hidden-style")) {
             portuguese_page.classList.remove("hidden-style");
+      };
     });
-
-  }
+  };
 
 };
 
